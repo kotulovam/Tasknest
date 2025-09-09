00a01cc10b920574e1866e7ed0619fb9c520c0b2
Michaela Kotuľová <kotulova.michaela271@gmail.com>
2025-09-09 12:53:28 +0200

    fix: prevent empty homepage flash before login redirect

--- app/page.js
+++ app/page.js
@@ -6,9 +6,10 @@ import { CirclePlus } from "lucide-react"
 import AddItemModal from "@/app/components/AddItemModal"
 import ToDo from "./todo-list/page"
 
-const App = () => {
+export default function App() {
   const [showModal, setShowModal] = useState(false);
   const [user, setUser] = useState(null)
+  const [loading, setLoading] = useState(true)
 
   useEffect(() => {
     fetch("/api/auth/current-user")
@@ -23,20 +24,43 @@ const App = () => {
       .catch((err) => {
         console.error("Error fetching user:", err);
         window.location.href = "/log-in";
+      })
+      .finally(() => {
+        setLoading(false);
       });
   }, []);
 
+  if (!user) {
+    return null;
+  }
+
   return (
     <div className="flex flex-col items-center space-y-3">
-
-      <div className="relative w-[95%]" >
-        <button onClick={() => setShowModal(true)} className="sticky w-full flex justify-center gap-3 items-center bg-purple-700 hover:bg-purple-600 text-slate-100 font-semibold py-3 px-6 rounded-xl shadow"><CirclePlus className="w-6 h-6" /> Add Task</button>
-      </div>
-      <div className="w-[95%]">
-        <ToDo />
-        {showModal && <AddItemModal setShowModal={setShowModal} itemName={"Task"} collection={"todos"} />} </div>
+      {loading ? (
+        <div>Loading...</div>
+      ) : (
+        <>
+          <div className="relative w-[95%]">
+            <button
+              onClick={() => setShowModal(true)}
+              className="sticky w-full flex justify-center gap-3 items-center bg-purple-700 hover:bg-purple-600 text-slate-100 font-semibold py-3 px-6 rounded-xl shadow"
+            >
+              <CirclePlus className="w-6 h-6" /> Add Task
+            </button>
+          </div>
+          <div className="w-[95%]">
+            <ToDo />
+            {showModal && (
+              <AddItemModal
+                setShowModal={setShowModal}
+                itemName={"Task"}
+                collection={"todos"}
+              />
+            )}
+          </div>
+        </>
+      )}
     </div>
   )
 }
 
-export default App
